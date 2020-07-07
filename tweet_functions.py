db6aec726885ec4cd0e4a56120fad6309c686655
jdsmithwes <53444854+jdsmithwes@users.noreply.github.com>
2020-07-06 22:48:17 -0400

    Update tweet_functions.py

--- tweet_functions.py
+++ tweet_functions.py
@@ -6,74 +6,90 @@ Created on Sun Jul  5 14:24:29 2020
 @author: jamaalsmith
 """
 
-import tweepy 
-  
-# Fill the X's with the credentials obtained by  
-# following the above mentioned procedure. 
-consumer_key = "SZQPcpQeruvMDoQuowCOk4fwd" 
-consumer_secret = "603EU5V98i7XI1bY6EKHfjiid0N88opdbR4eJYWQvdEM5AJNuE"
-access_key = "22245392-WGVcnhQ8OiYoAbfT2uFaW2XY0eo8uyG0Jj71DUGdy"
-access_secret = "rgdQTW32JQ39JMzCvDJsbMXHek0ilo3myol5ZlddXI7bw"
-  
-# Function to extract tweets 
-def get_tweets_user(username,number_of_tweets): 
-    """ This function pulls a designated number of tweets from a user"""
-          
-        # Authorization to consumer key and consumer secret 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
-  
-        # Access to user's access key and access secret 
-    auth.set_access_token(access_key, access_secret) 
-  
-        # Calling api 
-    api = tweepy.API(auth) 
-  
-        # 200 tweets to be extracted 
-    number_of_tweets=number_of_tweets
-    tweets = api.user_timeline(screen_name=username) 
-  
-        # Empty Array 
-    tmp=[]  
-  
-        # create array of tweet information: username,  
-        # tweet id, date/time, text 
-    tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created  
-    for j in tweets_for_csv: 
-  
-            # Appending tweets to the empty array tmp 
-        tmp.append(j)  
-  
-        # Printing the tweets 
-        print(tmp) 
+#influenced by Medium article: 'Scraping Tweets with Tweepy'
 
-
-# Function to extract tweets based on query 
-def get_tweets_query(query,rpp): 
-    """ This function pulls a designated number of tweets from a query"""
-          
-        # Authorization to consumer key and consumer secret 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
-  
-        # Access to user's access key and access secret 
-    auth.set_access_token(access_key, access_secret) 
-  
-        # Calling api 
-    api = tweepy.API(auth) 
-  
-        # 200 tweets to be extracted 
-    number_of_tweets=number_of_tweets
-    tweets = api.search(q=query,lang='en',rpp=rpp)
-  
-        # Empty Array 
-    tmp=[]  
-  
-        # create array of tweet information: username,  
-        # tweet id, date/time, text 
-    tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created  
-    for j in tweets_for_csv: 
-  
-            # Appending tweets to the empty array tmp 
-        tmp.append(j)  
-  
-        # Printing the tweets 
-        print(tmp) 
+def scraptweets(search_words, date_since, numTweets, numRuns):
+    
+    # Define a for-loop to generate tweets at regular intervals
+    # We cannot make large API call in one go. Hence, let's try T times
+    
+    # Define a pandas dataframe to store the date:
+    db_tweets = pd.DataFrame(columns = ['username', 'acctdesc', 'location', 'following',
+                                        'followers', 'totaltweets', 'usercreatedts', 'tweetcreatedts',
+                                        'retweetcount', 'text', 'hashtags']
+                                )
+    program_start = time.time()
+    for i in range(0, numRuns):
+        # We will time how long it takes to scrape tweets for each run:
+        start_run = time.time()
+        
+        # Collect tweets using the Cursor object
+        # .Cursor() returns an object that you can iterate or loop over to access the data collected.
+        # Each item in the iterator has various attributes that you can access to get information about each tweet
+        tweets = tweepy.Cursor(api.search, q=search_words, lang="en", since=date_since, tweet_mode='extended').items(numTweets)
+# Store these tweets into a python list
+        tweet_list = [tweet for tweet in tweets]
+# Obtain the following info (methods to call them out):
+        # user.screen_name - twitter handle
+        # user.description - description of account
+        # user.location - where is he tweeting from
+        # user.friends_count - no. of other users that user is following (following)
+        # user.followers_count - no. of other users who are following this user (followers)
+        # user.statuses_count - total tweets by user
+        # user.created_at - when the user account was created
+        # created_at - when the tweet was created
+        # retweet_count - no. of retweets
+        # (deprecated) user.favourites_count - probably total no. of tweets that is favourited by user
+        # retweeted_status.full_text - full text of the tweet
+        # tweet.entities['hashtags'] - hashtags in the tweet
+# Begin scraping the tweets individually:
+        noTweets = 0
+for tweet in tweet_list:
+# Pull the values
+            username = tweet.user.screen_name
+            acctdesc = tweet.user.description
+            location = tweet.user.location
+            following = tweet.user.friends_count
+            followers = tweet.user.followers_count
+            totaltweets = tweet.user.statuses_count
+            usercreatedts = tweet.user.created_at
+            tweetcreatedts = tweet.created_at
+            retweetcount = tweet.retweet_count
+            hashtags = tweet.entities['hashtags']
+try:
+    text = tweet.retweeted_status.full_text
+except AttributeError:
+    text=tweet.full_text
+    
+   # text = tweet.retweeted_status.full_text
+    #          text = tweet.full_text
+# Add the 11 variables to the empty list - ith_tweet:
+    ith_tweet = [username, acctdesc, location, following, followers, totaltweets,
+                         usercreatedts, tweetcreatedts, retweetcount, text, hashtags]
+# Append to dataframe - db_tweets
+db_tweets.loc[len(db_tweets)] = ith_tweet
+# increase counter - noTweets  
+            noTweets += 1
+        
+        # Run ended:
+        end_run = time.time()
+        duration_run = round((end_run-start_run)/60, 2)
+        
+        print('no. of tweets scraped for run {} is {}'.format(i + 1, noTweets))
+        print('time take for {} run to complete is {} mins'.format(i+1, duration_run))
+        
+        time.sleep(920) #15 minute sleep time
+# Once all runs have completed, save them to a single csv file:
+    from datetime import datetime
+    
+    # Obtain timestamp in a readable format
+    to_csv_timestamp = datetime.today().strftime('%Y%m%d_%H%M%S')
+# Define working path and filename
+    path = os.getcwd()
+    filename = path + '/data/' + to_csv_timestamp + '_sahkprotests_tweets.csv'
+# Store dataframe in csv with creation date timestamp
+    db_tweets.to_csv(filename, index = False)
+    
+    program_end = time.time()
+    print('Scraping has completed!')
+    print('Total time taken to scrap is {} minutes.'.format(round(program_end - program_start)/60, 2))
