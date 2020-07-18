870286894ea02993b5462c1a1deb979e5876d749
jdsmithwes <53444854+jdsmithwes@users.noreply.github.com>
2020-07-18 14:28:20 -0400

    update

--- tweet_functions.py
+++ tweet_functions.py
@@ -5,6 +5,9 @@ Created on Sun Jul  5 14:24:29 2020
 
 @author: jamaalsmith
 """
+import pandas as pd
+import numpy as np
+
 
 def clean_COVID_df(df):
     """Quick function to remove unneeded columns from the tweet dataframes"""
@@ -13,9 +16,9 @@ def clean_COVID_df(df):
     return df
 
 def write_exel(df, output_name):
-"""Quick function to write excel files from dataframes
-df = name of dataframe you want to write_exel
-output_name = name of file you want to write"""
+    """Quick function to write excel files from dataframes
+    df = name of dataframe you want to write_exel
+    output_name = name of file you want to write"""
     # create excel writer object
     writer = pd.ExcelWriter('covid_data.xlsx')
     # write dataframe to excel
@@ -23,3 +26,12 @@ output_name = name of file you want to write"""
     # save the excel
     writer.save()
     print('DataFrame is written successfully to Excel File.')
+
+#from gensim.parsing.preprocessing import remove_stopwords
+from pd import DataFrame
+def create_document_term_matrix(message_list, vectorizer):
+    from pandas import DataFrame
+    doc_term_matrix = vectorizer.fit_transform(message_list)
+    df = pd.DataFrame(doc_term_matrix.toarray(), columns= vectorizer.get_feature_names())
+
+    return df
