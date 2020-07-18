a9ff23e4f04436ebdbbc0d4dfcd71215334f8f99
jdsmithwes <53444854+jdsmithwes@users.noreply.github.com>
2020-07-18 16:14:54 -0400

    update

--- tweet_functions.py
+++ tweet_functions.py
@@ -28,10 +28,39 @@ def write_exel(df, output_name):
     print('DataFrame is written successfully to Excel File.')
 
 #from gensim.parsing.preprocessing import remove_stopwords
-from pd import DataFrame
+#from pd import DataFrame
 def create_document_term_matrix(message_list, vectorizer):
-    from pandas import DataFrame
+    #from pandas import DataFrame
     doc_term_matrix = vectorizer.fit_transform(message_list)
     df = pd.DataFrame(doc_term_matrix.toarray(), columns= vectorizer.get_feature_names())
 
     return df
+
+def tsne_plot(model):
+    "Creates and TSNE model and plots it. This function was originally found on Kaggle"
+    labels = []
+    tokens = []
+
+    for word in model.wv.vocab:
+        tokens.append(model[word])
+        labels.append(word)
+
+    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+    new_values = tsne_model.fit_transform(tokens)
+
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+
+    plt.figure(figsize=(16, 16))
+    for i in range(len(x)):
+        plt.scatter(x[i],y[i])
+        plt.annotate(labels[i],
+                     xy=(x[i], y[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
+    plt.show()
