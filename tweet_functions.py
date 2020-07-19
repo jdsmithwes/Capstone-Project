ed5393860aa1038b970f3e40ecea20a51329d88b
jdsmithwes <53444854+jdsmithwes@users.noreply.github.com>
2020-07-19 13:58:41 -0400

    update

--- tweet_functions.py
+++ tweet_functions.py
@@ -64,3 +64,27 @@ def tsne_plot(model):
                      ha='right',
                      va='bottom')
     plt.show()
+
+from sklearn.manifold import TSNE
+def display_closestwords_tsnescatterplot(model, word, size):
+
+    arr = np.empty((0,size), dtype='f')
+    word_labels = [word]
+    close_words = model.similar_by_word(word)
+    arr = np.append(arr, np.array([model[word]]), axis=0)
+    for wrd_score in close_words:
+        wrd_vector = model[wrd_score[0]]
+        word_labels.append(wrd_score[0])
+        arr = np.append(arr, np.array([wrd_vector]), axis=0)
+
+    tsne = TSNE(n_components=3, random_state=0)
+    np.set_printoptions(suppress=True)
+    Y = tsne.fit_transform(arr)
+    x_coords = Y[:, 0]
+    y_coords = Y[:, 1]
+    plt.scatter(x_coords, y_coords)
+    for label, x, y in zip(word_labels, x_coords, y_coords):
+        plt.annotate(label, xy=(x, y), xytext=(0, 0), textcoords='offset points')
+        plt.xlim(x_coords.min()+0.00005, x_coords.max()+0.00005)
+        plt.ylim(y_coords.min()+0.00005, y_coords.max()+0.00005)
+    plt.show()
