0429972fa04e294909e72992ca5f109d97c7f97c
jdsmithwes <53444854+jdsmithwes@users.noreply.github.com>
2020-07-28 23:47:38 -0400

    update

--- graphing_functions.py
+++ graphing_functions.py
@@ -1,6 +1,17 @@
 import plotly.io as pio
+import plotly.express as px
 
 def plotly_line (df,x,y):
     """Function that provides line graphs in Plotly Express"""
     fig = px.line(df,x=x, y=y)
+    fig.show()
+
+def plotly_scatter(df,x,y,trendline,color):
+    """Function for creating a scatterplot in Plotly Express"""
+    fig = px.scatter(df,x, y=y,color=color,trendline=trendline)
+    fig.show()
+
+def Plotly_bar(df,x,y,title,color):
+    """Function for creating a plotly express bar chart"""
+    fig = px.bar(df, x=x, y=y, title=title,color=color)
     fig.show()
