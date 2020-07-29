3d100aaff75c351eda6d8104319d5de0a7f1e30e
jdsmithwes <53444854+jdsmithwes@users.noreply.github.com>
2020-07-29 00:12:11 -0400

    update

--- credentials.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-consumer_key= 'SZQPcpQeruvMDoQuowCOk4fwd'
-
-consumer_secret= '603EU5V98i7XI1bY6EKHfjiid0N88opdbR4eJYWQvdEM5AJNuE'
-
-access_token= '22245392-WGVcnhQ8OiYoAbfT2uFaW2XY0eo8uyG0Jj71DUGdy'
-
-access_token_secret= 'rgdQTW32JQ39JMzCvDJsbMXHek0ilo3myol5ZlddXI7bw'
